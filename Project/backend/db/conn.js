9d5071155cbf7c1f24dd62a087970c430d1f6d6b
Randula Mawaththta <randula98@gmail.com>
2022-08-10 23:44:31 +0530

    Database connected

---
+++ Project/backend/db/conn.js
@@ -0,0 +1,26 @@
+const { MongoClient } = require('mongodb');
+require("dotenv").config({ path: "./.env" });
+
+const url = process.env.REACT_APP_BACKEND_ATLAS_URL;
+const frontend = new MongoClient(url, { 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true 
+});
+
+var _db;
+
+module.exports = {
+    connectToServer: function(callback) {
+        frontend.connect(function(err , db){
+            //verify the db object
+            if(db){
+                _db = db.db("synthetic")
+                console.log("Successfully connected to MongoDB")
+            }
+        })
+    },
+
+    getDb : function(){
+        return _db;
+    }
+};
---
+++ Project/backend/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config({ path: "./.env" });
+const port = process.env.REACT_APP_BACKEND_PORT || 5000;
+app.use(cors());
+app.use(express.json());
+
+//get driver connection
+const dbo = require("./db/conn");
+
+app.listen(port, () => {
+    //perform connection to database
+    dbo.connectToServer(function (err){
+        if (err) console.error(err);
+    });
+    console.log(`Server started on port ${port}`);
+});
+
