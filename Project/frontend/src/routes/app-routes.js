532c1bbd64b9a42039d546193e69165d92dd2a18
Randula Mawaththta <randula98@gmail.com>
2022-09-05 02:19:08 +0530

    Folders Rearranged

---
+++ Project/frontend/src/routes/app-routes.js
@@ -0,0 +1,21 @@
+//import './App.css';
+
+//import "dist/output.css";
+
+import React from 'react';
+// import { Route , Routes } from 'react-router-dom';
+
+import NavBar from '../components/navbar/navbar';
+
+function App() {
+  return (
+    <div>
+      <NavBar/>
+      {/* <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+      </Routes> */}
+    </div>
+  );
+}
+
+export default App;
