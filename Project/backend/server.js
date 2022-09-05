dd38af4c712b8937a58da9a561fff250d30f6a55
Sanodya Rajapaksha <it21504204@my.sliit.lk>
2022-09-06 01:15:01 +0530

    Implement target,promotion,discount backend

--- Project/backend/server.js
+++ Project/backend/server.js
@@ -6,6 +6,11 @@ const port = process.env.REACT_APP_BACKEND_PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+//get routes
+app.use("/target",require("./routes/target/record"));
+app.use("/discount",require("./routes/discount/record"));
+app.use("/promotion",require("./routes/promotion/record"));
+
 //get driver connection
 const dbo = require("./db/conn");
 
