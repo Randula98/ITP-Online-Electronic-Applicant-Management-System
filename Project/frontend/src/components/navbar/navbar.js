722d7731bb372e7e3d3e43d0ff2c6af08425f0ef
Randula Mawaththta <randula98@gmail.com>
2022-09-11 00:43:33 +0530

    Navbar UI Implemented

--- Project/frontend/src/components/navbar/navbar.js
+++ Project/frontend/src/components/navbar/navbar.js
@@ -68,7 +68,15 @@ function NavBar() {
 								href="/"
 								className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
 							>
-								About
+								Products
+							</a>
+						</li>
+						<li>
+							<a
+								href="/"
+								className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+							>
+								Brands
 							</a>
 						</li>
 						<li>
@@ -84,10 +92,56 @@ function NavBar() {
 								href="/"
 								className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
 							>
+								About
+							</a>
+						</li>
+						<li>
+							<a
+								href="/"
+								className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+							>
 								Contact
 							</a>
 						</li>
 					</ul>
+
+					<form>
+						<label
+							htmlFor="default-search"
+							className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-gray-300"
+						>
+							Search
+						</label>
+						<div className="relative mx-10 searchbar">
+							<input
+								type="search"
+								id="default-search"
+								className="block p-4 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+								placeholder="Search Items"
+								required=""
+							/>
+							<button
+								type="submit"
+								className=" button text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+							>
+								<svg
+									aria-hidden="true"
+									className="w-5 h-5 text-gray-500 dark:text-gray-400"
+									fill="none"
+									stroke="currentColor"
+									viewBox="0 0 24 24"
+									xmlns="http://www.w3.org/2000/svg"
+								>
+									<path
+										strokeLinecap="round"
+										strokeLinejoin="round"
+										strokeWidth="2"
+										d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+									></path>
+								</svg>
+							</button>
+						</div>
+					</form>
 				</div>
 			</div>
 		</nav>
