f7e2efdbd6c8dd5c62108d22924dd7546c965e95
Sanodya Rajapaksha <it21504204@my.sliit.lk>
2022-09-11 17:38:44 +0530

    Kitchen Item carousal teplate added

--- Project/frontend/src/pages/home/index.js
+++ Project/frontend/src/pages/home/index.js
@@ -2,12 +2,14 @@ import React from "react";
 
 import HeroSection from "./HeroSection";
 import OfferSection from "./OfferSection";
+import KitchenItemSection from "./KitchenItemSection";
 
 const Home = () => {
 	return (
 		<div>
 			<HeroSection />
 			<OfferSection />
+			<KitchenItemSection />
 		</div>
 	);
 };
