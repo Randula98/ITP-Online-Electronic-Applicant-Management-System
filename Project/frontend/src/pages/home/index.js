f108192703a1584d9ee1ab8d78ce09b555da75d9
Chanudi Tharushika <87629005+IT20029586@users.noreply.github.com>
2022-09-11 17:38:03 +0530

    LaptopsSection carousel section added

--- Project/frontend/src/pages/home/index.js
+++ Project/frontend/src/pages/home/index.js
@@ -2,12 +2,14 @@ import React from "react";
 
 import HeroSection from "./HeroSection";
 import OfferSection from "./OfferSection";
+import LaptopsSection from "./LaptopsSection";
 
 const Home = () => {
 	return (
 		<div>
 			<HeroSection />
 			<OfferSection />
+			<LaptopsSection />
 		</div>
 	);
 };
