86196a0a15da3989a82c84bf0ee24428dd88f37b
Randula Mawaththta <randula98@gmail.com>
2022-09-22 15:13:01 +0530

    Temp cus dash added

---
+++ Project/frontend/src/pages/cusdash/dash.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function CusDash() {
+	return (
+		<div>
+			<br />
+			<br />
+			<br />
+			<br />
+			<br />
+			<h1>Customer Dash</h1>
+		</div>
+	);
+}
