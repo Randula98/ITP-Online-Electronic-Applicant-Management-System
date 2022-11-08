4d24469e8dae24da0503ae55792e3cda7d72b08e
Chanudi Tharushika <87629005+IT20029586@users.noreply.github.com>
2022-11-08 16:07:02 +0530

    Fixed Employee Registration UI

--- Project/frontend/src/pages/register/empregister.js
+++ Project/frontend/src/pages/register/empregister.js
@@ -29,13 +29,6 @@ export default function EmpRegister() {
 			<br />
 			<br />
 			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
 			<section className="bg-gray-50 dark: cusregsec">
 				<div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0 cusregcard">
 					<a href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white regtext">
