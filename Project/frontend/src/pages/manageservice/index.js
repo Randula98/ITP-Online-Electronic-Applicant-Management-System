3692a09ab1faa134eaafd431acd51f6ff567846e
Roshan Rathnayaka <87793882+IT21132650@users.noreply.github.com>
2022-11-08 19:28:49 +0530

    Service Management Edit

--- Project/frontend/src/pages/manageservice/index.js
+++ Project/frontend/src/pages/manageservice/index.js
@@ -701,12 +701,12 @@ export default function ServiceManagement() {
                     </div>
                 </div >
                 <br />
-                <div className="row btnrow">
+                {/* <div className="row btnrow">
                     <a href="/manageservice/viewallrep" target="_blank"><button type="button"
                         className="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">View
                         All Repairs</button></a>
 
-                </div>
+                </div> */}
             </div>
 
             <div className="newRepairs">
