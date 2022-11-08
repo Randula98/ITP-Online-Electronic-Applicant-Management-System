96f1258fcc130316299960eab004966710cd4e22
Umanda Kumarasinghe <it21102028@my.sliit.lk>
2022-11-08 22:04:10 +0530

    loan UI bug fixed

--- Project/frontend/src/pages/managefinancial/index.js
+++ Project/frontend/src/pages/managefinancial/index.js
@@ -21,7 +21,7 @@ const RecordPendingLoans = (props) => (
             {props.record.loanpurpose}
         </td>
         <td class="py-4 px-6">
-            <a href={`/viewemployee/${props.record._id}`}><button type="button" class="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 shadow-lg shadow-purple-500/50 dark:shadow-lg dark:shadow-purple-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">View Profile</button></a>
+            <a href={`/viewemployee/${props.record.employeeid}`}><button type="button" class="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 shadow-lg shadow-purple-500/50 dark:shadow-lg dark:shadow-purple-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">View Profile</button></a>
         </td>
         <td class="py-4 px-6">
             <button onClick={() => {
@@ -49,7 +49,7 @@ const ApprovedLoansRecord = (props) => (
             {props.record.loanpurpose}
         </td>
         <td class="py-4 px-6">
-            <a href={`/viewemployee/${props.record._id}`}><button type="button" class="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 shadow-lg shadow-purple-500/50 dark:shadow-lg dark:shadow-purple-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">View Profile</button></a>
+            <a href={`/viewemployee/${props.record.employeeid}`}><button type="button" class="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 shadow-lg shadow-purple-500/50 dark:shadow-lg dark:shadow-purple-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">View Profile</button></a>
         </td>
         <td class="py-4 px-6">
             <button onClick={() => {
